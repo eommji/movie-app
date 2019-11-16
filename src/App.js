cf3a25449cc6571764df47ca5b1221d186dc0ab2
eommji <37979744+eommji@users.noreply.github.com>
2019-11-16 15:03:20 +0900

    add : rendering the movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React, { Component } from 'react';
-import Axios from 'axios';
+import React, { Component } from "react";
+import Axios from "axios";
+import Movie from "./Movie";
 
 class App extends Component {
   state = {
     isLoading: true,
     movies: []
-  }
+  };
 
-  getMovies = async() => {
-    const movies = await Axios.get('https://yts-proxy.now.sh/list_movies.json');
-  }
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await Axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
 
   componentDidMount() {
     this.getMovies();
   }
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
       <div>
-        <div>{isLoading ? 'loading' : 'ready'}</div>
+        {isLoading
+          ? "loading..."
+          : movies.map(e => (
+              <Movie
+                key={e.id}
+                id={e.id}
+                year={e.year}
+                title={e.title}
+                summary={e.summary}
+                poster={e.medium_cover_image}
+              />
+            ))}
       </div>
     );
   }
