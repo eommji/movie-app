a3c358e9164ce8c3e8bb693325ef78a7ea536522
eommji <37979744+eommji@users.noreply.github.com>
2019-11-16 10:03:44 +0900

    add : fetching movies from api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    movies: []
+  }
+
+  getMovies = async() => {
+    const movies = await Axios.get('https://yts-proxy.now.sh/list_movies.json');
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>
+        <div>{isLoading ? 'loading' : 'ready'}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
